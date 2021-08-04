a746a90f3fdfb8e06d8c786e29a0918c9dd8eda3
Edward <edward.arechwa@gogomedia.pl>
2021-08-04 08:55:37 +0200

    initial structure of project

---
+++ config/webpack.common.js
@@ -0,0 +1,74 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const PrettierPlugin = require('prettier-webpack-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
+
+const paths = require('./paths')
+
+module.exports = {
+  // Where webpack looks to start building the bundle
+  entry: [paths.src + '/index.ts'],
+
+  // Where webpack outputs the assets and bundles
+  output: {
+    path: paths.build,
+    filename: '[name].bundle.js',
+    publicPath: '/',
+  },
+
+  // Customize the webpack build process
+  plugins: [
+    // Removes/cleans build folders and unused assets when rebuilding
+    new CleanWebpackPlugin(),
+
+    // Copies files from target to destination folder
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: paths.src + '/assets',
+          to: 'assets',
+          globOptions: {
+            ignore: ['*.DS_Store'],
+          },
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+
+    // Generates an HTML file from a template
+    // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
+    new HtmlWebpackPlugin({
+      title: 'webpack Boilerplate',
+      template: paths.src + '/template.html', // template file
+      filename: 'index.html', // output file
+    }),
+
+    // ESLint configuration
+    new ESLintPlugin({
+      files: ['.', 'src', 'config'],
+      formatter: 'table',
+    }),
+
+    // Prettier configuration
+    new PrettierPlugin(),
+  ],
+
+  // Determine how modules within the project are treated
+  module: {
+    rules: [
+      { test: /\.ts$/, use: ['babel-loader'] },
+      { test: /\.js$/, use: ['babel-loader'] },
+      { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: 'asset/resource' },
+      { test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: 'asset/inline' },
+    ],
+  },
+
+  resolve: {
+    modules: [paths.src, 'node_modules'],
+    extensions: ['.tsx', '.ts', '.js', '.json'],
+    alias: {
+      '@': paths.src,
+    },
+  },
+}
